a7a26581a2b0d291cb57ff75aa09abb9d282e5c3
Nate Donato <natedonato@gmail.com>
2019-06-17 16:29:38 -0700

    adds webpack, html (prints answers to console)

--- jumble.js
+++ jumble.js
@@ -28,14 +28,22 @@ String.prototype.dejumbleSort = function(){
     return sortedDict[sorted];
 };
 
-function getcombinations(arr, k){
-    
-};
+document.addEventListener("DOMContentLoaded", () => {
+    let one = document.getElementById('1');
+    let two = document.getElementById('2');
+    let three = document.getElementById('3');
+    let four = document.getElementById('4');
+    let button = document.getElementById("button");
 
-
-
-
-console.log("itnga".dejumbleSort());
-console.log("kaoew".dejumbleSort());
-console.log("nucpha".dejumbleSort());
-console.log("otlbet".dejumbleSort());
+    button.addEventListener("click", () => {
+        let str1 = one.value;
+        let str2 = two.value;
+        let str3 = three.value;
+        let str4 = four.value;
+        
+        console.log(str1.dejumbleSort());
+        console.log(str2.dejumbleSort());
+        console.log(str3.dejumbleSort());
+        console.log(str4.dejumbleSort());
+    });
+});
